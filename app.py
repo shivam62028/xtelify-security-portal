6815a8ed2e19123f80ce08822312c7eaec745876
shivamvisualpath <shivamkumar620886@gmail.com>
2026-07-30 16:33:13 +0530

    Updated code and dist file

--- app.py
+++ app.py
@@ -2736,4 +2736,5 @@ async def sr(fp: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
