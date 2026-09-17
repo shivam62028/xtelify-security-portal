56eff2099ba3d499352cb493f7bf5330e8917dfd
shivam62028 <shivamkumar620886@gmail.com>
2026-09-17 15:48:55 +0530

    fixing resolution tracking

--- app.py
+++ app.py
@@ -3940,7 +3940,7 @@ async def ollama_status():
 async def smart_search(req: Request):
     """Smart search using Ollama to parse natural language queries"""
     try:
-        data = await req.json()
+        data = await req.json()do not name  it  "Manger closure report" it has to be depended on the container sub type. what i am selected in the "COntainer sub type". fix this .
         query = data.get('query', '')
 
         if not query:
